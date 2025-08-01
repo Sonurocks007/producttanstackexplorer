a49fe5eedb748855b5747a383488a478d882e754
Sonubgupta.hp@gmail.com <Sonubgupta.hp@gmail.com>
2025-08-01 12:39:43 +0530

    Initial setup with React, Tailwind, and TanStack Query

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className=" flex items-center justify-center w-full h-screen  bg-amber-50">
+      <h1 className="font bold text-3xl text-center">Product Explorer Project using TanStack.</h1>
+    </div>
+  );
+}
+
+export default App;
